116d8dfd3c399ba7bedd9f0f01a1a8c5b89b372d
TAn <tanvd357@gmail.com>
2024-01-29 11:50:12 +0700

    Test StackedHeader

--- StackedHeaderDGV/Form1.Designer.vb
+++ StackedHeaderDGV/Form1.Designer.vb
@@ -23,16 +23,41 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.e = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ee = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.e, Me.Column2, Me.ee})
         Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.Size = New System.Drawing.Size(776, 426)
         Me.DataGridView1.TabIndex = 0
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Parent.Child 1.Input 1"
+        Me.Column1.Name = "Column1"
+        '
+        'e
+        '
+        Me.e.HeaderText = "Parent.Child 1.Input 2"
+        Me.e.Name = "e"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Parent.Input 3"
+        Me.Column2.Name = "Column2"
+        '
+        'ee
+        '
+        Me.ee.HeaderText = "Parent.Input 4"
+        Me.ee.Name = "ee"
         '
         'Form1
         '
@@ -48,4 +73,8 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents e As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents ee As DataGridViewTextBoxColumn
 End Class
--- StackedHeaderDGV/Form1.vb
+++ StackedHeaderDGV/Form1.vb
@@ -1,3 +1,15 @@
-﻿Public Class Form1
+﻿Imports StackedHeaderDGV.StackedHeader
+
+Public Class Form1
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Dim objREnderer As New StackedHeaderDecorator(DataGridView1)
+
+    End Sub
 
 End Class
